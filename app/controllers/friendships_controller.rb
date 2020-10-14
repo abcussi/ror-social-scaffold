e7115435f52226471aece7323c4930c7456fa207
angel <cussi.angel.benjamin@gmail.com>
2020-10-14 18:45:27 -0400

    add view friendship index and new add controler method create and pass params add route freindship

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,9 +1,21 @@
 class FriendshipsController < ApplicationController
+
   def new
     @friendship = Friendship.new
   end
 
   def create
-    @friendship
+   @friendship = Friendship.new(friendship_params)
+   respond_to do |format|
+    if @friendship.save
+      format.html {redirect_to @friendship}
+    else
+      format.html{render 'new'}
+    end
+  end
+
+  private 
+  def friendship_params
+    params.require(:friendship).permit(:user_id, :request_id, :status)
   end
 end
