f694a63d6a6b6586cc1afe2ad2bde142e582ea5c
kamil <devkamilnaija@gmail.com>
2020-11-01 21:07:52 +0100

    ADDS ASSOCIATIONS TO USER MODEL

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,10 +15,22 @@ class User < ApplicationRecord
   # has_many :friends_requester, through: :friendships_requester, source: 'requested'
   # has_many :friends_requested, through: :friendships_requested, source: 'requester'
 
-  has_many :friendships
-  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'request_id'
+  #has_many :friendships
+  #has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'request_id'
 
   # Array of users who are already friends
+
+  has_many :friend_sent, class_name: 'Friendship', foreign_key: 'user_id', inverse_of: 'user', dependent: :destroy
+  has_many :friend_request, class_name: 'Friendship', foreign_key: 'request_id', inverse_of: 'request', dependent: :destroy
+  has_many :friends, -> { merge(Friendship.friends) }, through: :friend_sent, source: :request
+  has_many :pending_requests, -> { merge(Friendship.not_friends) }, through: :friend_sent, source: :request
+  has_many :received_requests, -> { merge(Friendship.not_friends) }, through: :friend_request, source: :user
+
+
+
+
+
+
   def friends
     friends_array = friendships.map { |friendship| friendship.request if friendship.confirmed }
     friends_array += inverse_friendships.map { |friendship| friendship.user if friendship.confirmed }
