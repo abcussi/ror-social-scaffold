36e263c49052f00016ed9ec4b6bf336f93332b12
kamil <devkamilnaija@gmail.com>
2020-11-01 21:19:00 +0100

    UPDATES ROUTE.rb FILE

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,15 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :users, only: [:index, :show]
+  resources :users, only: %i[index show] do
+    resources :friendships, only: %i[create] do
+      collection do
+        get 'accept_friend'
+        get 'decline_friend'
+      end
+    end
+  end
+
   resources :posts, only: [:index, :create] do
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]
