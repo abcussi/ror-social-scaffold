5b278794469a80b61db8658061f3e0ea6290be0c
kamil <devkamilnaija@gmail.com>
2020-11-05 13:32:25 +0100

    ADDS A METHOD TO CHECK SENT REQUESTS IN FRIENSHIP HELPER

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -2,4 +2,8 @@ module FriendshipsHelper
   def friend_request_sent?(user)
     current_user.friend_sent.exists?(request_id: user.id, status:nil)
   end
+
+  def friend_request_received?(user)
+    current_user.friend_request.exists?(request_id: user.id, status:nil)
+  end
 end
