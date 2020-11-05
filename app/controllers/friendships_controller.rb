726f8dda36019425ff45bb41a3fce6dbb0070650
kamil <devkamilnaija@gmail.com>
2020-11-05 13:10:19 +0100

    ADDS CREATE ACTION TO FRIENDSHIP CONTROLLER AND UPDATES USER's INDEX VIEW PAGE

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,22 +1,32 @@
 class FriendshipsController < ApplicationController
 
-  def new
-    @friendship = Friendship.new
-  end
+  # def new
+  #   @friendship = Friendship.new
+  # end
 
+  # def create
+  #  @friendship = Friendship.new(friendship_params)
+  #  respond_to do |format|
+  #   if @friendship.save
+  #     format.html {redirect_to @friendship}
+  #   else
+  #     format.html{render 'new'}
+  #   end
+  #   end
+  # end
+
+  # private 
+  # def friendship_params
+  #   params.require(:friendship).permit(:user_id, :request_id, :status)
+  # end
   def create
-   @friendship = Friendship.new(friendship_params)
-   respond_to do |format|
+    #@user = User.find(params[:user_id])
+    @friendship = current_user.friendships.build(request_id: params[:request_id])
+    
     if @friendship.save
-      format.html {redirect_to @friendship}
+      flash[:success] = 'Friend Request sent'
     else
-      format.html{render 'new'}
+      flash[:danger] = 'Friend Request Failed'
     end
-    end
-  end
-
-  private 
-  def friendship_params
-    params.require(:friendship).permit(:user_id, :request_id, :status)
   end
 end
